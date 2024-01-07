93e6153ec47852250d9968e1ed4e28cf630b470a
Krishna <113199449+Krishna01work@users.noreply.github.com>
2024-01-07 14:19:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,30 @@
-// create web server
-// 1. load modules
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-// 2. create web server
-var server = http.createServer(function(request,response){
-    // 2.1 get url
-    var parsedUrl = url.parse(request.url);
-    var resource = parsedUrl.pathname;
-    console.log('resource='+resource);
-    // 2.2 get query string as object
-    var query = qs.parse(parsedUrl.query);
-    console.log('query='+JSON.stringify(query));
-    // 2.3 routing
-    if(resource == '/'){
-        fs.readFile('index.html','utf-8',function(error,data){
-            if(error){
-                response.writeHead(500,{'Content-Type':'text/html'});
-                response.end('500 Internal Server '+error);
-            }else{
-                response.writeHead(200,{'Content-Type':'text/html'});
-                response.end(data);
-            }
-        });
-    }else if(resource == '/comments'){
-        if(request.method == 'GET'){
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.end(JSON.stringify(query));
-        }else if(request.method == 'POST'){
-            var body = '';
-            request.on('data',function(data){
-                body += data;
-            });
-            request.on('end',function(){
-                var post = qs.parse(body);
-                console.log('post='+JSON.stringify(post));
-                response.writeHead(200,{'Content-Type':'text/html'});
-                response.end(JSON.stringify(post));
-            });
-        }
-    }else{
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.end('404 Page Not Found');
-    }
+// Create web server
+// Run: node comments.js
+// Open browser: http://localhost:3000
+// 2nd option: nodemon comments.js
+// 3rd option: node-dev comments.js
+// 4th option: node comments.js
+// 5th option: npm start
+// 6th option: npm run dev
+
+const express = require('express');
+const app = express();
+
+// Load static files
+app.use(express.static('public'));
+
+// Load view engine
+app.set('view engine', 'ejs');
+
+// Listen to port
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
-// 3. start server
-server.listen(80,function(){
-    console.log('Server running at http://
-    
+
+// Load routes
+const index = require('./routes/index');
+const comments = require('./routes/comments');
+
+// Use routes
+app.use('/', index);
+app.use('/comments', comments);
